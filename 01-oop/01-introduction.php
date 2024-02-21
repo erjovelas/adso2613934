b6cc8890f2ba57ee960c0cc740c4eb5a8402027e
erjovelas <erikavelasquezvalencia@gmail.com>
2024-02-20 20:49:02 -0500

    se edita oop, se crean ejemplos de los tipos de programación

--- 01-oop/01-introduction.php
+++ 01-oop/01-introduction.php
@@ -18,9 +18,59 @@ if (password_verify($string, $hash)){
     echo "Verified Succesful!";
 }
 # Structured Programing
-
+function add($num1, $num2){
+    return $num1 + $num2;
+}
+echo add(2, 5);
 
 # Object Oriented  Programming
+Class Adition {
+    public $num1;
+    public $num2;
+
+    public function getResult(){
+        return ($this->num1 + $this->num2);
+    }
+}
+
+$sum = new Adition;
+$sum->num1 = 1024;
+$sum->num2 = 512;
+echo "<br>La suma de {$sum->num1} y {$sum->num2} es: " . $sum->getResult();
+
 ?>
 
+<main>
+        <form action="" method="post">
+            <label>
+                    <p>Number 1:</p>
+                    <input type="range" name="n1" step="1" value="0" max="50" oninput="o1.value=this.value">
+                    <output id="o1">0</output>
+           </label>
+           <label>
+                    <p>Number 2:</p>
+                    <input type="range" name="n2" step="1" value="0" max="50" oninput="o2.value=this.value">
+                    <output id="o2">0</output>
+           </label>
+           <button>Calcular</button>
+           <div class="Result">
+                <?php
+                  if($_POST){
+                    $sum = new Adition;
+                    $sum->num1 = $_POST['n1'];
+                    $sum->num2 = $_POST['n2'];
+                    echo "<br>La suma de {$sum->num1} y {$sum->num2} es: " . $sum->getResult();
+                }
+
+
+                ?>
+
+           </div>
+
+
+
+        </form>
+
+
+    </main>
 
