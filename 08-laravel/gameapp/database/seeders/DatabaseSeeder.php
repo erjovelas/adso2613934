bf9968a51c57a0e8f40c26964c042b2a2023760a
erjovelas <erikavelasquesvalencia@gmail.com>
2024-06-17 21:13:50 -0500

    se crean los models category, game y collection. Se complementan las creaciones. Se complementan los modelos, se organiza el user factory

--- 08-laravel/gameapp/database/seeders/DatabaseSeeder.php
+++ 08-laravel/gameapp/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+           Userseeder::class,
+           CategorySeeder::class,
+           GameSeeder::class,
         ]);
+        User::factory(50)->create();
+        
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
